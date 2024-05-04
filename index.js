cad0ac4c78120d699ee84162065211e9e9e1d2af
Mihir Chakma <27927615+mihirchakma@users.noreply.github.com>
2024-05-04 22:08:08 +0530

    initial project uploaded

---
+++ index.js
@@ -0,0 +1,23 @@
+const bodyEl = document.querySelector("body")
+
+bodyEl.addEventListener("mousemove", (event)=>{
+    // console.log("Moved");
+    const xPosition = event.offsetX;
+    const yPosition = event.offsetY;
+
+    const spanEl = document.createElement("span");
+
+    spanEl.style.left = xPosition + "px";
+    spanEl.style.top = yPosition + "px";
+
+    const size = Math.random() * 100;
+
+    spanEl.style.width = size + "px";
+    spanEl.style.height = size + "px";
+
+    bodyEl.appendChild(spanEl);
+
+    setTimeout(() => {
+        spanEl.remove();
+    }, 3000);
+})
